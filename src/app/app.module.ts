6cf53108cb0354f476269d6f072a1324daa211c7
yexx <675740900@qq.com>
2018-11-14 10:42:53 +0800

    添加Material Design组件，需要npm install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FightComponent } from './home/fight/fight.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -13,7 +14,8 @@ import { FightComponent } from './home/fight/fight.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FightComponent
+        FightComponent,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
